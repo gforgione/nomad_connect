06241122ff99b14cef5a1661b74fc8f9d675cef8
Jordan Deren <jordanderen1@gmail.com>
2021-03-01 18:25:41 +0000

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+jerry = User.new(first_name: "jerry", last_name: "derry", email: "jderry@aol.com", username: "jderry", password: "123456")
+jerry.save!
+gianboy = User.new(first_name: "gian", last_name: "luca", username: "gluca", password: "123456", email: "gluca@aol.com")
+gianboy.save!
+lisbon = City.new(name: "Lisbon")
+lisbon.save!
+loc = Location.new(user_id: jerry.id, city_id: lisbon.id)
+loc.save!
+lis = Forum.new(name: "Lis", group: "General", city: lisbon)
+lis.save!
+post = Post.new(user: jerry, forum: lis, content: "Where can i find cool bars in Lisbon?", title: "cool bars in lisbon") 
+post.save!
+comment = Comment.new(content: "I like the bar near Cascais", user: gianboy, post: post)
+comment.save!
+chatroom = Chatroom.new(name: "lis.gen", forum: lis)
+chatroom.save!
+message = Message.new(user: gianboy, content: "whats up guys", chatroom: chatroom)
+message.save!
+
