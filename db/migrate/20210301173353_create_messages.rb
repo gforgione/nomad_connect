ed1332d75361f20a6d62d1b03cff826e4c1a982c
Jordan Deren <jordanderen1@gmail.com>
2021-03-02 12:48:59 +0000

    done

--- db/migrate/20210301173353_create_messages.rb
+++ db/migrate/20210301173353_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-      t.text :content, null: false
+      t.string :content, null: false
       t.boolean :visible, null: false, default: true
       t.references :user, null: false, foreign_key: true
       t.references :chatroom, null: false, foreign_key: true
