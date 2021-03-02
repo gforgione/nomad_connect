daba8529f81bc4f1ab62e0fd2a9dbb7a2dd85d51
Jordan Deren <jordanderen1@gmail.com>
2021-03-02 11:40:42 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'cities#home'
+ 
+  resources :cities, only: %w[show] do 
+    resources :faq, only: %w[index]
+    resources :events, only: %w[index show]
+    resources :headlines, only: %w[index]
+    resources :forums, only: %w[index] do 
+      resources :posts, only: %w[index]
+    end 
+  end 
+  resources :forums, only: %w[index] do 
+    resources :posts, only: %w[create]
+  end 
+  resources :posts, only: %w[destroy] do 
+    resources :comments, only: %w[create]
+  end 
+  resources :chatrooms, only: %w[show] do
+    resources :messages, only: %w[create]
+  end 
+  resources :locations, only: %w[index]
 end
