d7579aab718d6dbfa664900b0087c016b4d3b5ff
Thales Gomes <thalessud@gmail.com>
2021-03-02 15:56:33 +0000

    usernames improved and many cities added to user

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,39 +9,42 @@ Chatroom.destroy_all
 Message.destroy_all
 
 puts 'Creating our beloved Jerry'
-jerry = User.create!(first_name: "jerry", last_name: "derry", email: "jderry@aol.com", username: "jderry", password: "123456")
 
 puts 'Creating Application Enviroment'  
-10.times do
+9.times do
   print '###########'
   user = User.create!(
     first_name: Faker::Name.first_name, 
     last_name: Faker::Name.last_name, 
     email: "#{Faker::Name.initials}@gmail.com", 
     password: "123456",
-    username: "#{ Faker::Name.initials }_#{(1..99).to_a.sample}"
+    username: "#{ Faker::Name.first_name }_#{(1..99).to_a.sample}"
     )
-  
-  city = City.create!(name: Faker::Address.city)
+  5.times do
+    city = City.create!(name: Faker::Address.city)
 
-  location = Location.create!(user_id: user.id, city_id: city.id)
+    location = Location.create!(user_id: user.id, city_id: city.id)
 
-  forum_categories = ["General", "Work", "Adventures"]
+    forum_categories = ["General", "Work", "Adventures"]
 
-  2.times do
-    forum = Forum.create!(name: Faker::Book.title, group: forum_categories.sample, city: city)
+    2.times do
+      forum = Forum.create!(name: Faker::Book.title, group: forum_categories.sample, city: city)
 
-    chatroom = Chatroom.create!(name: "#{ city.name } #{ forum_categories.sample }", forum: forum )
-    
-    5.times do
-      post = Post.create!(user: user, forum: forum, content: Faker::Quote.matz, title: Faker::Quote.singular_siegler)
+      chatroom = Chatroom.create!(name: "#{ city.name } #{ forum_categories.sample }", forum: forum )
+      
+      4.times do
+        post = Post.create!(user: user, forum: forum, content: Faker::Quote.matz, title: Faker::Quote.singular_siegler)
 
-      5.times do
-        comment = Comment.create!(content: Faker::Quote.matz, user: user, post: post)
-        message = Message.create!(user: user, content: Faker::Quote.matz, chatroom: chatroom)
+        3.times do
+          comment = Comment.create!(content: Faker::Quote.matz, user: user, post: post)
+          message = Message.create!(user: user, content: Faker::Quote.matz, chatroom: chatroom)
+        end
       end
     end
   end
 end
+
+jerry = User.create!(first_name: "jerry", last_name: "derry", email: "jderry@aol.com", username: "jderry", password: "123456")
+
 puts '#'
 puts 'Done!'
