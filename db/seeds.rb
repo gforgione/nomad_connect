25b05b78da7b90881e64f381a780a6a5c2e0f61d
Thales Gomes <thalessud@gmail.com>
2021-03-02 14:08:43 +0000

    Seeds finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,7 @@ Chatroom.destroy_all
 Message.destroy_all
 
 # gianboy = User.create!(first_name: "gian", last_name: "luca", username: "gluca", password: "123456", email: "gluca@aol.com")
-# lisbon = City.create!(name: "Lisbon")
-# loc = Location.create!(user_id: jerry.id, city_id: lisbon.id)
-# lis = Forum.create!(name: "Lis", group: "General", city: lisbon)
-# post = Post.create!(user: jerry, forum: lis, content: "Where can i find cool bars in Lisbon?", title: "cool bars in lisbon") 
-# comment = Comment.create!(content: "I like the bar near Cascais", user: gianboy, post: post)
-# chatroom = Chatroom.create!(name: "lis.gen", forum: lis)
-# message = Message.create!(user: gianboy, content: "whats up guys", chatroom: chatroom)
+
 
 puts 'Creating our beloved Jerry'
 jerry = User.create!(first_name: "jerry", last_name: "derry", email: "jderry@aol.com", username: "jderry", password: "123456")
@@ -28,18 +22,18 @@ puts 'Creating Application Enviroment'
     last_name: Faker::Name.last_name, 
     email: "#{Faker::Name.initials}@gmail.com", 
     password: "123456",
-    username: "#{first_name}#{(1..99).to_a.sample}"
+    username: "#{ Faker::Name.initials }_#{(1..99).to_a.sample}"
     )
   
   city = City.create!(name: Faker::Address.city)
 
   location = Location.create!(user_id: user.id, city_id: city.id)
 
-  FORUM_CATEGORIES = ["General", "Work" "Adventures"]
+  forum_categories = ["General", "Work" "Adventures"]
 
-  forum = Forum.create!(name: Faker::Book.title, group: FORUM_CATEGORIES.sample, city: city)
+  forum = Forum.create!(name: Faker::Book.title, group: forum_categories.sample, city: city)
 
-  chatroom = Chatroom.create!(name: "#{ city.name FORUM_CATEGORIES.sample }", forum: forum )
+  chatroom = Chatroom.create!(name: "#{ city.name} #{ forum_categories.sample }", forum: forum )
   
   10.times do
     post = Post.create!(user: user, forum: forum, content: Faker::Quote.matz, title: Faker::Quote.singular_siegler)
