37597b8703c58820771a29d714befbddb1bf603b
Giuseppe Forgione <giuseppeforgione11@gmail.com>
2021-03-08 13:00:56 +0000

    City image added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
     if object.photo.attachment.present?
       photo_key = object.photo.key
     else
-      photo_key = "https://res.cloudinary.com/ducqpxfso/image/upload/v1614857823/no-img-profile_og8ezm.jpg" #no photo image on cloudinary
+      photo_key = cl_image_path "q086ozr6xng829m1a5y0fcnv1wqs" #no photo image on cloudinary
     end 
  end
 end
