f817510a2410cefc07464443cb94eb4368047d86
Giuseppe Forgione <giuseppeforgione11@gmail.com>
2021-03-08 11:06:21 +0000

    helpers added, favicons added to forum groups

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,11 +7,21 @@ class PostsController < ApplicationController
     @post = Post.new
     @city = City.find(params[:city_id])
     @forums = Forum.where(city_id: @city)
-    @current_forum = Forum.find(params[:forum_id])
-    @chatroom = Chatroom.find_by(forum_id: @current_forum)
+    @chatroom = Chatroom.find_by(forum_id: current_forum)
     @message = Message.new
   end
 
+  def current_forum
+    @current_forum ||= Forum.find(params[:forum_id]) 
+    # special operator that sets this variable only if its not set yet. memoization
+    # dont want to repeat requests for no reason espcially database requests because that will bottleneck your app
+    # even if you call current forum 1000x times, it will just call this once
+    # you would use htis if you were access current_forum in a lot of places
+    # makes the instance variable accessible to your views and helpers
+  end
+
+  helper_method :current_forum # sometimes you can add this to application controller if you want to access it from whole app
+  
   def create
     
     @post = Post.new(post_params)
---
+++ app/helpers/forums_helper.rb
@@ -0,0 +1,25 @@
+module ForumsHelper
+  def forum_link(forum)
+    home = "fas fa-home"
+    work = "fas fa-briefcase"
+    adventure = "fas fa-hiking"
+    other = "fas fa-campground"
+
+    classes = forum == current_forum ? "forum-name font-weight-bold" : "forum-name"
+    forum_name = forum.group.downcase
+
+    if forum_name == "general"
+      font_awesome_class = home
+    elsif forum_name == "work" || forum_name == "professional"
+      font_awesome_class = work
+    elsif forum_name == "adventures" || forum_name == "social"
+      font_awesome_class = adventure
+    else
+      font_awesome_class = other
+    end
+    
+    link_to city_forum_posts_path(forum.city, forum), class: classes do 
+      content_tag(:i, '', class: font_awesome_class) + " " + forum.group
+    end
+  end
+end
