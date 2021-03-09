763fee2f950c2352ace64a4e9159e24c8f4fe6fe
Giuseppe Forgione <giuseppeforgione11@gmail.com>
2021-03-09 14:12:45 +0000

    navbar functionality and style improved. still problems with the dropdown section

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_03_03_211824) do
+ActiveRecord::Schema.define(version: 2021_03_05_144430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
