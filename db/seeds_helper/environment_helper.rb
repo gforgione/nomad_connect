98fdd51d2e1b99e4a44aa54a77665c36788c3875
Thales Gomes <thalessud@gmail.com>
2021-03-18 22:26:53 +0000

    new methods for DRY code added to environment_helper

--- db/seeds_helper/environment_helper.rb
+++ db/seeds_helper/environment_helper.rb
@@ -11,31 +11,19 @@ module EnvironmentHelper
     user = SeedsHelper.jerry_user
 
     6.times do
-      city = SeedsHelper.gen_city(@@cities, @@city_index, @@calendar_ids, @@photo_keys)
-      @@city_index += 1
-  
-      location = Location.create!(user_id: user.id, city_id: city.id)
-
-      @@forum_categories.each do |category|
-        SeedsHelper.gen_city_environment(category, city, user)
-      end
+      city = create_city
+      gen_location_city_env(city, user)
     end
   end
 
   def gen_model_1 # Cities with many users
     3.times do
       print '###############'
-      city = SeedsHelper.gen_city(@@cities, @@city_index, @@calendar_ids, @@photo_keys)
-      @@city_index += 1
+      city = create_city
 
       6.times do
         user = SeedsHelper.gen_user
-    
-        location = Location.create!(user_id: user.id, city_id: city.id)
-        
-        @@forum_categories.each do |category|
-          SeedsHelper.gen_city_environment(category, city, user)
-        end
+        gen_location_city_env(city, user)
       end
     end
   end
@@ -46,15 +34,25 @@ module EnvironmentHelper
       user = SeedsHelper.gen_user
 
       3.times do
-        city = SeedsHelper.gen_city(@@cities, @@city_index, @@calendar_ids, @@photo_keys)
-        @@city_index += 1
-    
-        location = Location.create!(user_id: user.id, city_id: city.id)
-
-        @@forum_categories.each do |category|
-          SeedsHelper.gen_city_environment(category, city, user)
-        end
+        city = create_city
+        gen_location_city_env(city, user)
       end
+    end
+  end
+
+  private
+
+  def self.create_city
+    city = SeedsHelper.gen_city(@@cities, @@city_index, @@calendar_ids, @@photo_keys)
+    @@city_index += 1
+    city
+  end
+
+  def self.gen_location_city_env(city, user)
+    location = Location.create!(user_id: user.id, city_id: city.id)
+
+    @@forum_categories.each do |category|
+      SeedsHelper.gen_city_environment(category, city, user)
     end
   end
 end
